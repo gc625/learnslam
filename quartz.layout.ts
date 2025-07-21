bc17a249087f1e0a97474c584de15f5fe453a076
gc625 <gabrielchan625@gmail.com>
2025-07-21 11:41:41 +0800

    add comments

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+  Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'jgc625/learnslam',
+      // from data-repo-id
+      repoId: 'R_kgDOPOy_kA',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDOPOy_kM4CtOZ0',
+      // from data-lang
+      lang: 'en'
+    }
+  }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/gc625/learnslam"
